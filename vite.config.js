a7d20d1af5ab562466ed30ef56e3e55c0d86915e
Reshma <reshma4sheys2@gmail.com>
2026-01-28 17:10:00 +0530

    Update Vite config base and package.json scripts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// nEW CODE
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Netlify automatically sets process.env.NETLIFY to true.
+  // If we are on Netlify, use '/'. If we are anywhere else (GitHub), use '/ASGMT-10/'.
+  base: process.env.NETLIFY ? '/' : '/ASGMT-11-React-Multi-Page/',
 })
